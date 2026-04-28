0bd55735007c21881817ecfedc183271718dfbcd
Evgeny Bashun <bashun97@gmail.com>
2026-04-28 21:45:37 +0300

    ggh

--- FirstLibrary/Package.swift
+++ FirstLibrary/Package.swift
@@ -5,20 +5,34 @@ import PackageDescription
 
 let package = Package(
     name: "FirstLibrary",
+	platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "FirstLibrary",
-            targets: ["FirstLibrary"]),
+			targets: ["FirstLibrary"]
+		),
+		.library(name: "SecondLibrary", targets: ["SecondLibrary"])
     ],
 	dependencies: [
-		.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0")
+		.package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.0"),
+		.package(url: "https://github.com/onevcat/Kingfisher.git", from: "8.8.0")
 	],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "FirstLibrary"),
+			name: "FirstLibrary",
+			dependencies: [
+				.product(name: "Kingfisher", package: "Kingfisher")
+			]
+		),
+		.target(
+			name: "SecondLibrary",
+			dependencies: [
+				.product(name: "Alamofire", package: "Alamofire")
+			]
+		),
         .testTarget(
             name: "FirstLibraryTests",
             dependencies: ["FirstLibrary"]
---
+++ FirstLibrary/Sources/SecondLibrary/SecondLibrary.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  FirstLibrary
+//
+//  Created by Евгений Башун on 28.04.2026.
+//
+
+import Foundation
+
+public enum LibSecond {
+	case one
+}
